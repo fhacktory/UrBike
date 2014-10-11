f2446f1f33fbfb2a1c81bd5f429f05b5763e2801
Rebouh Aymen <aymenworks@gmail.com>
2014-10-11 17:02:06 +0200

    -Ajout d'extrait de localisation de l'utilisateur -Ajout de vérification de la connectivité de l'utilisateur -Ajout du projet open source SCLAlertView pour utiliser des popup de tout types ( alert, success, .. )

--- UrBike/UrBike/App/AppDelegate.swift
+++ UrBike/UrBike/App/AppDelegate.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
-import Foundation
+import MapKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
 
     var window: UIWindow?
-    var currentReachability: Reachability?
+    var currentReachability: Reachability!
+    var isReachable: Bool!
+    var userLocation: LocationToolBox?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        LocationToolBox.setupUserLocation()
         
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "getLocation", userInfo: nil, repeats: true)
+
         return true
     }
-
+    
+    func getLocation() {
+        LocationToolBox.getLocation()
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
